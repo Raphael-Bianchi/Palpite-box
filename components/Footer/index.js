663e3aa3283718ebe54f0f1a9f0c6b12b302b44e
Raphael Bianchi De Vicente <raphael.direto@gmail.com>
2022-03-29 22:15:00 -0300

    Initial Version

---
+++ components/Footer/index.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <div className='bg-gray-700 p-4'>
+      <div className='container mx-auto text-center font-bold text-white'>
+        Projeto desenvolvido por: Raphael Bianchi /
+        <a className='hover:underline' href='https://www.linkedin.com/in/raphaelengenharia/'> Linkedin </a>/
+        <a className='hover:underline' href='https://github.com/Raphael-Bianchi'> GitHub </a>
+        <div className='mt-2'>
+          <img className='inline p-4' src='/logo_semana_fsm.png' />
+          <img className='inline p-4' src='/logo_devpleno.png' />
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Footer
